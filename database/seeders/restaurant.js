876f715019b1dedda38f26d055b576da675f1aac
William Maguire <billymaguire3@gmail.com>
2020-10-18 01:51:45 -0400

    Seed 10Mil restaurants and Copy from csv to restaurants table in dishes database

--- database/seeders/restaurant.js
+++ database/seeders/restaurant.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-console */
+const fs = require('fs');
 const faker = require('faker');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = require('csv-write-stream');
+
+const writer = csvWriter();
 
 // Helpers ------------------------------------------
 const generateRestaurantName = () => {
   const suffixArray = ['Place', 'Joint', 'Grill', 'Parlor', 'Steakhouse', 'Burgers', 'Deli',
-    'Comfort House', 'Vegan', 'Sweets', 'Escape', 'Kitchen'];
-  return faker.name.firstName() + faker.random.arrayElement(suffixArray);
+    'Comfort House', 'Vegan', 'Sweets', 'Escape', 'Kitchen', 'Bistro', 'Eatery', 'BBQ'];
+  return `${faker.name.firstName()}'s ${faker.random.arrayElement(suffixArray)}`;
 };
 
 const generatePhoneNumber = () => faker.phone.phoneNumberFormat();
@@ -15,42 +18,66 @@ const generateRandomCity = () => faker.address.city();
 const generateRandomState = () => faker.address.stateAbbr();
 const generateRandomZip = () => faker.address.zipCode();
 
-const csvWriter = createCsvWriter({
-  path: './database/data-storage/restaurants.csv',
-  header: [
-    { id: 'restaurant_id', title: 'restaurant_id' },
-    { id: 'name', title: 'name' },
-    { id: 'phone', title: 'phone' },
-    { id: 'email', title: 'email' },
-    { id: 'city', title: 'city' },
-    { id: 'state', title: 'state' },
-    { id: 'zip', title: 'zip' },
-  ],
-});
-
-const recordsGenerator = (start, end) => {
-  const result = [];
-  for (let i = start; i <= end; i += 1) {
-    if (i === 50 || i === 100 || i === 150) {
-      console.log(`Successfully seeded up to ${i} records.`);
+const dataGenerator = async () => {
+  const createRestaurantsRecords = async () => {
+    writer.pipe(fs.createWriteStream('./database/data-storage/restaurants.csv'));
+    for (let i = 1; i <= 10000000; i += 1) {
+      if (i % 500000 === 0) {
+        console.log(`Successfully seeded ${i} records`);
+      }
+      writer.write({
+        restaurant_id: i,
+        name: generateRestaurantName(),
+        phone: generatePhoneNumber(),
+        email: generateRandomEmail(),
+        city: generateRandomCity(),
+        state: generateRandomState(),
+        zip: generateRandomZip(),
+      });
     }
-    const restaurant = {
-      restaurant_id: i,
-      name: generateRestaurantName(),
-      phone: generatePhoneNumber(),
-      email: generateRandomEmail(),
-      city: generateRandomCity(),
-      state: generateRandomState(),
-      zip: generateRandomZip(),
-    };
-    result.push(restaurant);
-  }
-  return result;
+    writer.end();
+    console.log('Successfully Generated 10Mil Restaurant Records into csv');
+  };
+  await createRestaurantsRecords();
 };
+dataGenerator();
 
-const restaurantRecordsBuilder = recordsGenerator(1, 200);
+// const csvWriter = createCsvWriter({
+//   path: './database/data-storage/restaurants.csv',
+//   header: [
+//     { id: 'restaurant_id', title: 'restaurant_id' },
+//     { id: 'name', title: 'name' },
+//     { id: 'phone', title: 'phone' },
+//     { id: 'email', title: 'email' },
+//     { id: 'city', title: 'city' },
+//     { id: 'state', title: 'state' },
+//     { id: 'zip', title: 'zip' },
+//   ],
+// });
 
-csvWriter.writeRecords(restaurantRecordsBuilder) // returns a promise
-  .then(() => {
-    console.log('Done Seeding.');
-  });
+// const recordsGenerator = (start, end) => {
+//   const result = [];
+//   for (let i = start; i <= end; i += 1) {
+//     if (i === 50 || i === 100 || i === 150) {
+//       console.log(`Successfully seeded up to ${i} records.`);
+//     }
+//     const restaurant = {
+//       restaurant_id: i,
+//       name: generateRestaurantName(),
+//       phone: generatePhoneNumber(),
+//       email: generateRandomEmail(),
+//       city: generateRandomCity(),
+//       state: generateRandomState(),
+//       zip: generateRandomZip(),
+//     };
+//     result.push(restaurant);
+//   }
+//   return result;
+// };
+
+// const restaurantRecordsBuilder = recordsGenerator(1, 200);
+
+// csvWriter.writeRecords(restaurantRecordsBuilder) // returns a promise
+//   .then(() => {
+//     console.log('Done Seeding.');
+//   });
